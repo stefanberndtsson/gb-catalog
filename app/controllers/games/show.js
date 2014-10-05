53e59c7139342753a0a794fa61cae572792d3da5
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-10-05 21:46:48 +0200

    Writable note field

--- app/controllers/games/show.js
+++ app/controllers/games/show.js
@@ -3,5 +3,17 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     cartridges: function() {
 	return this.get('model').get('cartridges');
-    }.property('model.cartridges')
+    }.property('model.cartridges'),
+    actions: {
+	updateNote: function() {
+	    var controller = this;
+	    this.get('model').save().then(function(newModel) {
+		controller.set('model', newModel);
+		controller.set('editingNote', !newModel.get('note'));
+	    });
+	},
+	editNote: function() {
+	    this.set('editingNote', true);
+	}
+    }
 });
--- app/routes/games/show.js
+++ app/routes/games/show.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model: function(params) {
 	return this.store.find('game', params.id);
+    },
+    setupController: function(controller, model) {
+	controller.set('model', model);
+	controller.set('editingNote', !model.get('note'));
     }
 });
