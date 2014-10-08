b7a7a5b37d3a76fcf0075b41b6095f669bef468b
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-10-08 21:42:33 +0200

    Remove experimental adapter/serializer and handle list model specialities in the backend instead

--- app/adapters/game_list.js
+++
@@ -1,9 +0,0 @@
-import DS from "ember-data";
-
-export default DS.ActiveModelAdapter.extend({
-    host: "http://localhost:3012",
-    buildURL: function() {
-	console.log("buildURL");
-	return this.host+"/games";
-    }
-});
--- app/serializers/game_list.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.ActiveModelSerializer.extend({
-    extractArray: function(store, type, payload) {
-	payload = {game_lists: payload.games};
-	return this._super(store, type, payload);
-    }
-});
